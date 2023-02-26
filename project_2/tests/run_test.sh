0a2b87faa33d0e22c66412d7a787c1a391690fef
currocam <campuzanocurro@gmail.com>
2023-02-26 10:32:07 +0000

    test: bash for loop

--- project_2/tests/run_test.sh
+++ project_2/tests/run_test.sh
@@ -1,86 +1,17 @@
-# First case
-python main.py global-linear tests/case1/seq1.fasta \
-    tests/case1/seq2.fasta tests/case1/file.conf --print-alignment -o tmp.fasta
-bash tests/scripts/cmp.sh tmp.fasta tests/expected/case1.fasta
+for i in {1..4}
+do
+for algo in global-linear global-affine
+do
+echo Testing case $i: for $algo
+python main.py $algo --print-alignment tests/case$i/seq1.fasta \
+    tests/case$i/seq2.fasta tests/$algo.conf -o tmp.fasta
+bash tests/scripts/cmp.sh tmp.fasta tests/expected/$algo\_$i.fasta
 rm tmp.fasta
-
-
-python main.py global-linear tests/case1/seq1.fasta \
-    tests/case1/seq2.fasta tests/case1/file.conf -o tmp.fasta
-head -n 1 tests/expected/case1.fasta > tmp2.txt
-bash tests/scripts/cmp.sh tmp.fasta tmp2.txt
-rm tmp.fasta tmp2.txt
-
-python main.py global-linear-linspace tests/case1/seq1.fasta \
-    tests/case1/seq2.fasta tests/case1/file.conf -o tmp.fasta
-head -n 1 tests/expected/case1.fasta > tmp2.txt
-bash tests/scripts/cmp.sh tmp.fasta tmp2.txt
-rm tmp.fasta tmp2.txt
-
-python main.py global-affine tests/case1/seq1.fasta \
-    tests/case1/seq2.fasta tests/case1/affine.conf --print-alignment -o tmp.fasta
-bash tests/scripts/cmp.sh tmp.fasta tests/expected/case1_affine.fasta
+done;
+algo="global-linear-linspace"
+echo Testing case $i: for $algo
+python main.py $algo tests/case$i/seq1.fasta \
+     tests/case$i/seq2.fasta tests/$algo.conf -o tmp.fasta
+bash tests/scripts/cmp.sh tmp.fasta tests/expected/$algo\_$i.fasta
 rm tmp.fasta
-
-# Second case
-python main.py global-linear tests/case2/seq1.fasta \
-    tests/case2/seq2.fasta tests/case2/file.conf --print-alignment -o tmp.fasta
-bash tests/scripts/cmp.sh tmp.fasta tests/expected/case2.fasta
-rm tmp.fasta
-
-python main.py global-linear tests/case2/seq1.fasta \
-    tests/case2/seq2.fasta tests/case2/file.conf -o tmp.fasta
-head -n 1 tests/expected/case2.fasta > tmp2.txt
-bash tests/scripts/cmp.sh tmp.fasta tmp2.txt
-rm tmp.fasta tmp2.txt
-
-python main.py global-linear-linspace tests/case2/seq1.fasta \
-    tests/case2/seq2.fasta tests/case2/file.conf -o tmp.fasta
-head -n 1 tests/expected/case2.fasta > tmp2.txt
-bash tests/scripts/cmp.sh tmp.fasta tmp2.txt
-rm tmp.fasta tmp2.txt
-
-python main.py global-affine tests/case2/seq1.fasta \
-    tests/case2/seq2.fasta tests/case2/affine.conf --print-alignment -o tmp.fasta
-bash tests/scripts/cmp.sh tmp.fasta tests/expected/case2_affine.fasta
-rm tmp.fasta 
-
-# Third case
-python main.py global-linear tests/case3/seq1.fasta \
-    tests/case3/seq2.fasta tests/case3/file.conf --print-alignment -o tmp.fasta
-bash tests/scripts/cmp.sh tmp.fasta tests/expected/case3.fasta
-rm tmp.fasta
-
-python main.py global-linear tests/case3/seq1.fasta \
-    tests/case3/seq2.fasta tests/case3/file.conf -o tmp.fasta
-head -n 1 tests/expected/case3.fasta > tmp2.txt
-bash tests/scripts/cmp.sh tmp.fasta tmp2.txt
-rm tmp.fasta tmp2.txt
-
-python main.py global-linear-linspace tests/case3/seq1.fasta \
-    tests/case3/seq2.fasta tests/case3/file.conf -o tmp.fasta
-head -n 1 tests/expected/case3.fasta > tmp2.txt
-bash tests/scripts/cmp.sh tmp.fasta tmp2.txt
-rm tmp.fasta tmp2.txt
-
-python main.py global-affine tests/case3/seq1.fasta \
-    tests/case3/seq2.fasta tests/case3/affine.conf -o tmp.fasta
-head -n 1 tests/expected/case3_affine.fasta > tmp2.txt
-bash tests/scripts/cmp.sh tmp.fasta tmp2.txt
-rm tmp.fasta tmp2.txt
-
-#Fourth case (too many alignments)
-python main.py global-linear tests/case4/seq1.fasta \
-    tests/case4/seq2.fasta tests/case4/file.conf -o tmp.fasta
-bash tests/scripts/cmp.sh tmp.fasta tests/expected/case4.fasta
-rm tmp.fasta
-
-python main.py global-linear-linspace tests/case4/seq1.fasta \
-    tests/case4/seq2.fasta tests/case4/file.conf -o tmp.fasta
-bash tests/scripts/cmp.sh tmp.fasta tests/expected/case4.fasta
-rm tmp.fasta
-
-python main.py global-affine tests/case4/seq1.fasta \
-    tests/case4/seq2.fasta tests/case4/affine.conf -o tmp.fasta
-bash tests/scripts/cmp.sh tmp.fasta tests/expected/case4_affine.fasta
-rm tmp.fasta
+done
