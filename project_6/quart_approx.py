36d97946f2c6dab238206a02271c75a6e24275d6
Curro Campuzano <campuzanocurro@gmail.com>
2023-05-02 18:33:25 +0200

    not working

--- project_6/quart_approx.py
+++ project_6/quart_approx.py
@@ -1,3 +1,8 @@
+from collections import OrderedDict
+
+FORWARD, LEFT, RIGHT = "f", "l", "r"
+
+
 def find_even_odd(hp: str) -> tuple[list[int], list[int]]:
     """
     >>> find_even_odd("HHH")
@@ -17,7 +22,7 @@ def find_even_odd(hp: str) -> tuple[list[int], list[int]]:
 def match_pairs(x: list[int], y: list[int]) -> dict[int, int]:
     matchs = dict()
     for a, b in zip(x, reversed(y)):
-        if a > b:
+        if a + 1 >= b:
             break
         matchs[a] = b
     return matchs
@@ -28,7 +33,7 @@ def get_match_even(evens: list[int], odds: list[int]) -> dict[int, int]:
     >>> get_match_even([0, 2], [1, 3])
     {0: 3}
     >>> get_match_even([0, 4, 6], [1, 3, 5, 7])
-    {0: 7, 4: 5}
+    {0: 7}
     """
     return match_pairs(evens, odds)
 
@@ -38,10 +43,51 @@ def get_match_odds(evens: list[int], odds: list[int]) -> dict[int, int]:
     >>> get_match_odds([0, 2], [1, 3])
     {1: 2}
     >>> get_match_odds([0, 4, 6], [1, 3, 5, 7])
-    {1: 6, 3: 4}
+    {1: 6}
     """
     return match_pairs(odds, evens)
 
 
-evens, odds = find_even_odd("HHPHHHHH")
+def make_turn(path: list[int]) -> None:
+    n = len(path)
+    if n == 1:
+        return path
+    path[(n // 2) : (n // 2 + 2)] = [LEFT, LEFT]
+    return path
+
+
+def make_turn(path: list[int]) -> list[int]:
+    n = len(path)
+    if n == 1:
+        return RIGHT
+    path[(n // 2) : (n // 2 + 2)] = [RIGHT, RIGHT]
+    return path
+
+
+hp = "ppphhpphhhhpphhhphhphhphhhhpppppppphhhhhhpphhhhhhppppppppphphhphhhhhhhhhhhpphhhphhphpphphhhpppppphhh".upper()
+evens, odds = find_even_odd(hp)
 match_evens, match_odds = get_match_even(evens, odds), get_match_odds(evens, odds)
+
+if len(match_evens) > len(match_odds):
+    matchs = list(OrderedDict(sorted(match_evens.items())).items())
+else:
+    matchs = list(OrderedDict(sorted(match_odds.items())).items())
+
+path = (len(hp) - 1) * [FORWARD]
+
+k, v = matchs.pop()
+path[k:v] = make_turn(path[k:v])
+split = (v - k) // 2 + k
+upper_count, lower_count = 0, 0
+while matchs:
+    k, v = matchs.pop()
+    diff = (split - k - upper_count) - (v - split - 1 + lower_count)
+    if diff < 0:
+        lower_count -= diff
+        path[v + diff - 2 : v + (diff + 2)] = [LEFT, RIGHT, RIGHT, LEFT]
+    if diff > 0:
+        upper_count += diff
+        path[k + diff - 2 : k + diff + 2] = [LEFT, RIGHT, RIGHT, LEFT]
+
+
+"".join(path)
